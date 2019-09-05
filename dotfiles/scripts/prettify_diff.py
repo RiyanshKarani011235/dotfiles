5b1ddecb9866f5dc8c36bbf6120451e100426e4e
riyanshkarani011235 <riyansh.karani.011235@gmail.com>
2019-09-05 10:55:08 -0700

    prettify_diff now supports multiple file formats

--- dotfiles/scripts/prettify_diff.py
+++ dotfiles/scripts/prettify_diff.py
@@ -9,8 +9,16 @@ import os
 
 files = subprocess.check_output(['git', 'diff', '--name-only', 'master']).split('\n')
 
+SUPPORTED_FILE_FORMATS = ['tsx', 'ts']
+
+def valid_file_extension(file_name):
+    for fmt in SUPPORTED_FILE_FORMATS:
+        if file_name.endswith(fmt):
+            return True
+    return False
+
 # TODO(riyanshk): accept file extension types as sysargv
 prettify_command = 'npx prettier --write'
-for f in [f for f in files if f.endswith('tsx')]:
+for f in [f for f in files if valid_file_extension(f)]:
     prettify_command = prettify_command + ' {}'.format(f)
 os.system(prettify_command)
